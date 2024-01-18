92df27db79cdc2a05500518a412dcdfe8afc4952
Grzegorz Błaszczuk <jan.nowak@wp.pl>
2024-01-18 16:59:04 +0100

    New colsole.logs

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("Hello Word!");
 console.log('Hello Word 2!');
 console.log('Hello Word 3');
+console.log('Hello Word 4');
+console.log('Hello Word 5');
+console.log('Hello Word 6');
+console.log('Hello Word 7');
+console.log('Hello Word 8');
